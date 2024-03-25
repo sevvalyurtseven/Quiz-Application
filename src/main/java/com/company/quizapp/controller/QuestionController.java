a112307cbd5ca592c15303cdbc55e407088ac73b
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-25 16:35:23 +0300

    Added and improved question handling and responses in our system, making it easier to manage questions and get clear API feedback.

--- src/main/java/com/company/quizapp/controller/QuestionController.java
+++ src/main/java/com/company/quizapp/controller/QuestionController.java
@@ -2,7 +2,9 @@ package com.company.quizapp.controller;
 
 import com.company.quizapp.entity.Question;
 import com.company.quizapp.service.QuestionService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,17 +16,17 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("/allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
     @GetMapping("/category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category ){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category ){
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("/add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String>addQuestion(@RequestBody Question question){
        return questionService.addQuestion(question);
     }
 
