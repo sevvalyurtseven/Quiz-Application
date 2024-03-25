0c521ab555d2dc13a607cb3cd87f079f8d20b3d9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-25 13:19:33 +0300

    created QuestionService class with getAllQuestions method using QuestionDao for fetching questions

--- src/main/java/com/company/quizapp/service/QuestionService.java
+++ src/main/java/com/company/quizapp/service/QuestionService.java
@@ -1,7 +1,18 @@
 package com.company.quizapp.service;
 
+import com.company.quizapp.dao.QuestionDao;
+import com.company.quizapp.entity.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
+@Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions(){
+return questionDao.findAll();
+    }
 }
