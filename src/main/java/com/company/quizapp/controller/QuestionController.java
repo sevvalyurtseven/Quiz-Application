df49365ff075dd4cfc71d9220637ceb58b9b42db
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-25 09:43:32 +0300

    add QuestionService dependency to QuestionController with using @Autowired annotation

---
+++ src/main/java/com/company/quizapp/controller/QuestionController.java
@@ -0,0 +1,16 @@
+package com.company.quizapp.controller;
+
+import com.company.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+    
+}
