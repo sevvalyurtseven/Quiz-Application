07b13d336d0528402e0c4758fd5287a488af9452
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-25 17:50:07 +0300

    create QuizDao interface for Quiz entity data management

---
+++ src/main/java/com/company/quizapp/dao/QuizDao.java
@@ -0,0 +1,7 @@
+package com.company.quizapp.dao;
+
+import com.company.quizapp.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizDao extends JpaRepository<Quiz, Integer> {
+}
