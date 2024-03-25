0e349641953213e7f46d5f0e09fdf44dd046212e
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-25 18:54:51 +0300

    add getQuizQuestions method in QuizService for fetching quiz details

--- src/main/java/com/company/quizapp/service/QuizService.java
+++ src/main/java/com/company/quizapp/service/QuizService.java
@@ -3,13 +3,16 @@ package com.company.quizapp.service;
 import com.company.quizapp.dao.QuestionDao;
 import com.company.quizapp.dao.QuizDao;
 import com.company.quizapp.entity.Question;
+import com.company.quizapp.entity.QuestionWrapper;
 import com.company.quizapp.entity.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -27,5 +30,17 @@ public ResponseEntity<String> createQuiz(String category, int numQ, String title
     quizDao.save(quiz);
 
     return new ResponseEntity<>("Success", HttpStatus.CREATED);
+}
+
+public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id){
+    Optional<Quiz> quiz = quizDao.findById(id);
+    List<Question> questionsFromDB = quiz.get().getQuestions();
+    List<QuestionWrapper> questionsForUser = new ArrayList<>();
+    for(Question q : questionsFromDB){
+        QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+        questionsForUser.add(qw);
+    }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
 }
 }
