29acf0c4e1c773170c0474b1c4e6d9ceae814e20
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-25 18:54:04 +0300

    add GET endpoint '/get/{id}' in QuizController for fetching questions of a specific quiz. It uses QuizService to deliver questions in QuestionWrapper for a tidy and clear response

--- src/main/java/com/company/quizapp/controller/QuizController.java
+++ src/main/java/com/company/quizapp/controller/QuizController.java
@@ -1,13 +1,15 @@
 package com.company.quizapp.controller;
 
+import com.company.quizapp.dao.QuestionDao;
+import com.company.quizapp.entity.Question;
+import com.company.quizapp.entity.QuestionWrapper;
 import com.company.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -20,5 +22,10 @@ public class QuizController {
     @PostMapping("/create")
     public ResponseEntity<String>createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
 return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
     }
 }
