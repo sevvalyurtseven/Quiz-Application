4f92d1fac705e93151b32043abce6b28aba4dfad
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-25 13:21:53 +0300

    change string return type of allQuestions endpoint to List<Question>

--- src/main/java/com/company/quizapp/QuestionController.java
+++
@@ -1,7 +0,0 @@
-package com.company.quizapp;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class QuestionController {
-}
--- src/main/java/com/company/quizapp/controller/QuestionController.java
+++ src/main/java/com/company/quizapp/controller/QuestionController.java
@@ -1,10 +1,13 @@
 package com.company.quizapp.controller;
 
+import com.company.quizapp.entity.Question;
 import com.company.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/question")
@@ -13,7 +16,7 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("/allQuestions")
-    public String getAllQuestions(){
+    public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 }
